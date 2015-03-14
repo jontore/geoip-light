970447f51933733d7d7e0d6d1e4b08e65653051f
Jon Tore Hafstad <jontore@gmail.com>
2015-03-14 15:57:59 +0100

    turn on LED if country_code is not matched

--- geoip-light/geoip-light.ino
+++ geoip-light/geoip-light.ino
@@ -9,11 +9,14 @@
 #include <espduino.h>
 #include <rest.h>
 
+#define LIGHTPIN 13
+
 #define SSID ""
 #define PASSWORD ""
 
 #define DOMAIN "freegeoip.net"
 #define PATH "/json/"
+#define COUNTRY_CODE "CH"
 SoftwareSerial debugPort(2, 3); // RX, TX
 
 ESP esp(&Serial, &debugPort, 4);
@@ -27,16 +30,26 @@ void wifiCb(void* response)
   uint32_t status;
   RESPONSE res(response);
 
-  if(res.getArgc() == 1) {
+  if (res.getArgc() == 1) {
     res.popArgs((uint8_t*)&status, 4);
-    if(status == STATION_GOT_IP) {
+    if (status == STATION_GOT_IP) {
       debugPort.println("WIFI CONNECTED");
-     
       wifiConnected = true;
     } else {
       wifiConnected = false;
     }
-    
+
+  }
+}
+
+void turnOnLedIfNoMatch(String str, String matchStr) {
+  int matchIndex = str.indexOf(matchStr);  
+  debugPort.println(str);
+  debugPort.println(matchIndex);
+  if (matchIndex == -1) {
+    digitalWrite(LIGHTPIN, HIGH); 
+  } else {
+    digitalWrite(LIGHTPIN, LOW); 
   }
 }
 
@@ -44,16 +57,17 @@ void setup() {
   Serial.begin(19200);
   debugPort.begin(19200);
   debugPort.println("INIT");
+  pinMode(LIGHTPIN, OUTPUT);
   esp.enable();
   delay(500);
   esp.reset();
   delay(500);
-  while(!esp.ready());
+  while (!esp.ready());
 
   debugPort.println("ARDUINO: setup rest client");
-  if(!rest.begin(DOMAIN)) {
+  if (!rest.begin(DOMAIN)) {
     debugPort.println("ARDUINO: failed to setup rest client");
-    while(1);
+    while (1);
   }
 
   /*setup wifi*/
@@ -62,15 +76,14 @@ void setup() {
   esp.wifiConnect(SSID, PASSWORD);
   debugPort.println("ARDUINO: system started");
 }
-
-void loop() {
-  char response[266];
+char response[50] = "{country_code: CH}";
+void loop() { 
   esp.process();
-  if(wifiConnected) {
+  if (wifiConnected) {
     rest.get(PATH);
-    if(rest.getResponse(response, 266) == HTTP_STATUS_OK){
-      debugPort.println(response);
+    if (rest.getResponse(response, 50) == HTTP_STATUS_OK) {
       debugPort.println("ARDUINO: GET successful");
+      turnOnLedIfNoMatch(response, COUNTRY_CODE);
     }
     delay(10000);
   }
